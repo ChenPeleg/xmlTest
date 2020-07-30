1e4bf6515dd8ea241f2381cdbc34a36927f73a8f
ChenPeleg <chenpeleg@gmail.com>
2020-07-30 10:20:32 +0300

    refactoring code with danny

--- Program.cs
+++ Program.cs
@@ -25,37 +25,40 @@ namespace xmlToXls
             XmlNode xmlData = sampleXmlData.FirstChild;
             string templatePath = Path.GetFullPath(Path.Combine(path, @"..\..\GraphTemplate.xlsx"));
             FileInfo template = new FileInfo(templatePath);
-            createXlsx(xmlData, template);
+            //ExcelPackage excelpackage = createXlsx(xmlData, template);
+            Random rnd = new Random();
+            int rand = rnd.Next(1, 10000);
+            string fileName = "יצוא בדיקות מעבדה" + rand.ToString() + ".xlsx";
 
+            createXlsx(xmlData, template, fileName);
 
         }
-
-
-        private static void createXlsx(XmlNode xmlData, FileInfo template)
+        private static void createXlsx(XmlNode xmlData, FileInfo template, String fileName)
         {
-            XmlNode headers = xmlData.SelectSingleNode("Header");
-            XmlNode rows = xmlData.SelectSingleNode("Rows");
+            XmlNode headerData = xmlData.SelectSingleNode("Header");
+            XmlNode rowsData = xmlData.SelectSingleNode("Rows");
             List<string> headerCaptions = new List<string>();
             List<string> headerValues = new List<string>();
-            List<string> tableHeaders = new List<string>();
+            List<string> tableCaptions = new List<string>();
             List<List<string>> tableRows = new List<List<string>>();
-            string[,] rowsArray = new string[,] { { "" }, { "" } };
+
+            // Convert data from XML Structure to List structure
             if (xmlData.HasChildNodes)
             {
 
-                foreach (XmlNode node in headers)
+                foreach (XmlNode nodeHeaderField in headerData)
                 {
 
-                    headerCaptions.Add(node.Attributes["Caption"].Value);
-                    headerValues.Add(node.Attributes["Value"].Value);
+                    headerCaptions.Add(nodeHeaderField.Attributes["Caption"].Value);
+                    headerValues.Add(nodeHeaderField.Attributes["Value"].Value);
                 }
-                // seting table headers from the first row
-                foreach (XmlNode node in rows.FirstChild)
+                // seting table captions from the first row data
+                foreach (XmlNode nodeTableCaption in rowsData.FirstChild)
                 {
-                    tableHeaders.Add(node.Attributes["Caption"].Value);
+                    tableCaptions.Add(nodeTableCaption.Attributes["Caption"].Value);
                 }
                 // seting table rows as list of lists
-                foreach (XmlNode rowNode in rows)
+                foreach (XmlNode rowNode in rowsData)
                 {
                     List<string> oneRow = new List<string>();
                     foreach (XmlNode oneRowNode in rowNode)
@@ -70,20 +73,14 @@ namespace xmlToXls
 
             }
 
-            string path = $"{AppDomain.CurrentDomain.BaseDirectory}";
-            string outpudirPath = Path.GetFullPath(Path.Combine(path, @"..\..\SampleOutPut"));
-            Utils.OutputDir = new DirectoryInfo(outpudirPath);
 
             //$"{AppDomain.CurrentDomain.BaseDirectory}SampleApp"
-
-
-
-            string fileName = String.Join("- ", headerValues);
-            Random rnd = new Random();
-            int rand = rnd.Next(1, 10000);
-            fileName = fileName + rand + ".xlsx";
-            fileName = fileName.Replace("/", "-").Replace("\\", "-").
-                Replace(@"\", "-").Replace("//", "-");
+            /*  string fileName = String.Join("- ", headerValues);
+              Random rnd = new Random();
+              int rand = rnd.Next(1, 10000);
+              fileName = fileName + rand + ".xlsx";
+              fileName = fileName.Replace("/", "-").Replace("\\", "-").
+                  Replace(@"\", "-").Replace("//", "-");*/
 
             //Template path from library : $"{AppDomain.CurrentDomain.BaseDirectory}GraphTemplate.xlsx"
             using (ExcelPackage p = new ExcelPackage(template, true))
@@ -92,11 +89,11 @@ namespace xmlToXls
                 //default for sheets is 1 for dotnetcore
                 ExcelWorksheet ws = p.Workbook.Worksheets[1];
                 // first row headers
-                for (int i = 0; i < tableHeaders.Count; i++)
+                for (int i = 0; i < tableCaptions.Count; i++)
                 {
                     int cell = i + 1;
                     int firstRow = 1;
-                    ws.Cells[firstRow, cell].Value = tableHeaders[i];
+                    ws.Cells[firstRow, cell].Value = tableCaptions[i];
                 }
                 for (int r = 0; r < tableRows.Count; r++)
                 {
@@ -116,23 +113,23 @@ namespace xmlToXls
                         {
                             ws.Cells[row, cell].Value = value;
                         };
-
-
-
                     }
-
                 }
-
-
-
-
-                Byte[] bin = p.GetAsByteArray(); //Get the documet as a byte array from the stream and save it to disk.  (This is useful in a webapplication) ... 
-
-                FileInfo file = Utils.GetFileInfo(fileName);  //return file.FullName;
-                File.WriteAllBytes(file.FullName, bin);
-
-                Process.Start(Utils.OutputDir.FullName);
+                saveFile(p, fileName);
             }
+
+
+        }
+        private static void saveFile(ExcelPackage excelP, string fileName)
+        {
+
+            string path = $"{AppDomain.CurrentDomain.BaseDirectory}";
+            string outpudirPath = Path.GetFullPath(Path.Combine(path, @"..\..\SampleOutPut"));
+            Utils.OutputDir = new DirectoryInfo(outpudirPath);
+            Byte[] bin = excelP.GetAsByteArray(); //Get the documet as a byte array from the stream and save it to disk.  (This is useful in a webapplication) ... 
+            FileInfo file = Utils.GetFileInfo(fileName);  //return file.FullName;
+            File.WriteAllBytes(file.FullName, bin);
+            Process.Start(Utils.OutputDir.FullName);
 
         }
         private static void Log(string text)
