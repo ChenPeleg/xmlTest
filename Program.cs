61850e86131425f4385b8a9161179e2061a5871c
ChenPeleg <chenpeleg@gmail.com>
2020-07-30 09:39:28 +0300

    seperating into 2 functions

--- Program.cs
+++ Program.cs
@@ -18,26 +18,34 @@ namespace xmlToXls
         static void Main(string[] args)
         {
 
-            XmlDocument xmlData = new XmlDocument();
+            XmlDocument sampleXmlData = new XmlDocument();
             string path = $"{AppDomain.CurrentDomain.BaseDirectory}";
             string xmlPath = Path.GetFullPath(Path.Combine(path, @"..\..\data.xml"));
-            xmlData.Load(xmlPath);
-            XmlNode root = xmlData.FirstChild;
- 
+            sampleXmlData.Load(xmlPath);
+            XmlNode xmlData = sampleXmlData.FirstChild;
+            string templatePath = Path.GetFullPath(Path.Combine(path, @"..\..\GraphTemplate.xlsx"));
+            FileInfo template = new FileInfo(templatePath);
+            createXlsx(xmlData, template);
 
-            XmlNode headers = root.SelectSingleNode("Header");
-            XmlNode rows = root.SelectSingleNode("Rows");
+
+        }
+
+
+        private static void createXlsx(XmlNode xmlData, FileInfo template)
+        {
+            XmlNode headers = xmlData.SelectSingleNode("Header");
+            XmlNode rows = xmlData.SelectSingleNode("Rows");
             List<string> headerCaptions = new List<string>();
             List<string> headerValues = new List<string>();
             List<string> tableHeaders = new List<string>();
             List<List<string>> tableRows = new List<List<string>>();
             string[,] rowsArray = new string[,] { { "" }, { "" } };
-            if (root.HasChildNodes)
+            if (xmlData.HasChildNodes)
             {
 
                 foreach (XmlNode node in headers)
                 {
-                    Log(node.Attributes["Caption"].Value);
+
                     headerCaptions.Add(node.Attributes["Caption"].Value);
                     headerValues.Add(node.Attributes["Value"].Value);
                 }
@@ -62,17 +70,18 @@ namespace xmlToXls
 
             }
 
+            string path = $"{AppDomain.CurrentDomain.BaseDirectory}";
             string outpudirPath = Path.GetFullPath(Path.Combine(path, @"..\..\SampleOutPut"));
             Utils.OutputDir = new DirectoryInfo(outpudirPath);
+
             //$"{AppDomain.CurrentDomain.BaseDirectory}SampleApp"
 
-            string templatePath = Path.GetFullPath(Path.Combine(path, @"..\..\GraphTemplate.xlsx"));
-            FileInfo template =
-                new FileInfo(templatePath);
+
+
             string fileName = String.Join("- ", headerValues);
             Random rnd = new Random();
             int rand = rnd.Next(1, 10000);
-            fileName = fileName + rand +  ".xlsx";
+            fileName = fileName + rand + ".xlsx";
             fileName = fileName.Replace("/", "-").Replace("\\", "-").
                 Replace(@"\", "-").Replace("//", "-");
 
@@ -96,19 +105,32 @@ namespace xmlToXls
                     for (int c = 0; c < thisRow.Count; c++)
                     {
                         int cell = c + 1;
-                        ws.Cells[row, cell].Value = thisRow[c];
+                        var value = thisRow[c];
+                        decimal decimalValue;
+
+                        if (decimal.TryParse(value, out decimalValue))
+                        {
+                            ws.Cells[row, cell].Value = decimalValue;
+                        }
+                        else
+                        {
+                            ws.Cells[row, cell].Value = value;
+                        };
+
+
+
                     }
 
                 }
 
 
 
-                //Get the documet as a byte array from the stream and save it to disk.  (This is useful in a webapplication) ... 
-                Byte[] bin = p.GetAsByteArray();
 
-                FileInfo file = Utils.GetFileInfo(fileName);
+                Byte[] bin = p.GetAsByteArray(); //Get the documet as a byte array from the stream and save it to disk.  (This is useful in a webapplication) ... 
+
+                FileInfo file = Utils.GetFileInfo(fileName);  //return file.FullName;
                 File.WriteAllBytes(file.FullName, bin);
-                //return file.FullName;
+
                 Process.Start(Utils.OutputDir.FullName);
             }
 
